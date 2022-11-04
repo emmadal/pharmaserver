678f840f02c70a4f01b269ad5676895f56ce8b1a
emmadal <emmanueldalougou@gmail.com>
2022-11-04 18:09:31 +0000

    first commit

---
+++ handler.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+const { ApolloServer } = require("@apollo/server");
+const {
+  startServerAndCreateLambdaHandler,
+} = require("@as-integrations/aws-lambda");
+
+const resolvers = require("./src/resolvers.js");
+console.log(process.env)
+
+const server = new ApolloServer({
+  typeDefs: fs.readFileSync("./src/schema.graphql", { encoding: "utf-8" }),
+  resolvers,
+  introspection: process.env.NODE_ENV !== 'production'
+});
+
+// This final export is important!
+exports.graphqlHandler = startServerAndCreateLambdaHandler(server);
