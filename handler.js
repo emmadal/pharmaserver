8d1fae64de863218ff50df86c6fe562a6632de82
emmadal <emmanueldalougou@gmail.com>
2022-11-05 00:48:59 +0000

    mongodb connection successful

--- handler.js
+++ handler.js
@@ -3,14 +3,24 @@ const { ApolloServer } = require("@apollo/server");
 const {
   startServerAndCreateLambdaHandler,
 } = require("@as-integrations/aws-lambda");
+const mongoose = require("mongoose");
 
 const resolvers = require("./src/resolvers.js");
 
 const server = new ApolloServer({
   typeDefs: fs.readFileSync("./src/schema.graphql", { encoding: "utf-8" }),
   resolvers,
-  introspection: process.env.NODE_ENV !== 'production'
+  introspection: process.env.NODE_ENV !== "production",
+  csrfPrevention: true,
 });
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    keepAlive: true,
+    serverSelectionTimeoutMS: 9000,
+  })
+  .then(() => console.log("Connected to Database 🥳"))
+  .catch((err) => console.log('DB Error: ', err.message));
 
 // This final export is important!
 exports.graphqlHandler = startServerAndCreateLambdaHandler(server);
